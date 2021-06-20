f839603af7197f9cb5c951842796e77bbe3df048
zhoujian <jian2.zhou@rootcloud.com>
2021-06-20 10:27:57 +0800

    b1 modify

--- src/main/java/com/rootcloud/git/gitTest.java
+++ src/main/java/com/rootcloud/git/gitTest.java
@@ -4,5 +4,6 @@ public class gitTest {
   public static void main(String[] args) {
     System.out.println("hello git2");
     System.out.println("hello b1");
+    System.out.println("b1 测试");
   }
 }
