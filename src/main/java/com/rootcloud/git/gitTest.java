29ee89a1a08d86ece987bf53a07c656e231ee2e3
zhoujian <jian2.zhou@rootcloud.com>
2021-06-20 10:25:27 +0800

    master modify

--- src/main/java/com/rootcloud/git/gitTest.java
+++ src/main/java/com/rootcloud/git/gitTest.java
@@ -4,5 +4,8 @@ public class gitTest {
   public static void main(String[] args) {
     System.out.println("hello git2");
     System.out.println("hello b1");
+    System.out.println("b1 test");
+    System.out.println("b1 test first");
+    System.out.println("master test");
   }
 }
