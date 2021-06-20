532f4c9d659a593f86a28b54a36ee237518fe13f
zjHero <86149253+zjHero@users.noreply.github.com>
2021-06-20 11:09:57 +0800

    pull commit

--- src/main/java/com/rootcloud/git/gitTest.java
+++ src/main/java/com/rootcloud/git/gitTest.java
@@ -9,5 +9,6 @@ public class gitTest {
     System.out.println("b1 test first");
     System.out.println("master test");
     System.out.println("push test");
+    System.out.println("pull test");
   }
 }
